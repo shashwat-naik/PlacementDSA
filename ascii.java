6d702bb7b2a66177683e693cc0dabdcb9592fa85
shashwat-naik <naikshashwat3@gmail.com>
2025-02-03 23:44:42 +0530

    Fun change :)

--- ascii.java
+++ ascii.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ascii {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a character: ");
+        System.out.print("Enter character: ");
         char c = sc.next().charAt(0);
         int ascii = c;
 
