85f333f04ce7039a4462c08af9212b5d70d23a3f
shashwat-naik <naikshashwat3@gmail.com>
2025-03-08 21:52:26 +0530

    PYQ 15 incomplete

---
+++ Companies/TCS/TCS Upgrade/PYQ15.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PYQ15 {
+    // Q1 Minimum boats required
+
+    // public static int numRescueBoat(int[] people, int limit) {
+    //     int boatCount = 0;
+    //     Arrays.sort(people);
+
+    //     int left = 0;
+    //     int right = people.length - 1;
+
+    //     while (left <= right) {
+    //         if (people[left] + people[right] <= limit) {
+    //             left++;  
+    //         }
+    //         right--;  
+    //         boatCount++;
+    //     }
+    //     return boatCount;
+    // }
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+        
+    //     System.out.print("Enter number of people: ");
+    //     int n = sc.nextInt();
+    //     int[] people = new int[n];
+
+    //     System.out.println("Enter weights of people: ");
+    //     for (int i = 0; i < n; i++) {
+    //         people[i] = sc.nextInt();
+    //     }
+
+    //     System.out.print("Enter weight limit per boat: ");
+    //     int limit = sc.nextInt();
+
+    //     System.out.println("Minimum boats required: " + numRescueBoat(people, limit));
+
+    //     sc.close();
+    // }
+
+    // Q2 Doctor consultation fee
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x, count=0, flag=0, feeCollect=0;
+        while(sc.hasNextInt()){
+            x=sc.nextInt();
+        }
+        sc.close();
+    }
+
+}
