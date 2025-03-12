25ebed8560ad91aa91595e582e5fc7511b7a76fb
shashwat-naik <naikshashwat3@gmail.com>
2025-03-12 23:23:12 +0530

    YT1 Q1

---
+++ Companies/TCS/YT1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class YT1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+        sc.close();
+    }
+}
