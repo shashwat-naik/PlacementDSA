c6c2d777be99c6d34d0cbd249cad25ce99623127
shashwat-naik <naikshashwat3@gmail.com>
2025-03-12 23:46:11 +0530

    YT1 Q2

--- Companies/TCS/YT1.java
+++ Companies/TCS/YT1.java
@@ -14,9 +14,19 @@ public class YT1 {
             map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
         }
 
+        // Q1 For printing each element with frequency
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
+
+        // Q2 For printing the element with maximum frequency
+        for (int x : map.keySet()) {
+            if (map.get(x) > n / 2) {
+                System.out.println(x);
+                return;
+            }
+        }
+        System.out.println(-1);
         sc.close();
     }
 }
